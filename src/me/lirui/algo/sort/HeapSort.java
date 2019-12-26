35d52bd43c04297b971d91c2a191f67030ea35dd
李瑞 <lirui.sep@gmail.com>
2019-12-26 23:09:22 +0800

    * update .gitignore config file * remove useless files

--- src/me/lirui/algo/sort/HeapSort.java
+++
@@ -1,4 +0,0 @@
-package me.lirui.algo.sort;
-
-/** Created by RichardLee on 2017/4/4. */
-public class HeapSort {}
--- src/me/lirui/algo/sort/QuickSort.java
+++
@@ -1,4 +0,0 @@
-package me.lirui.algo.sort;
-
-/** Created by RichardLee on 2017/4/3. */
-public class QuickSort {}
