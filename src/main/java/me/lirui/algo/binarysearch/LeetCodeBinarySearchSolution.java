358fd5484aba0145a0ee2ad9392aad43e73dedf8
李瑞 <lirui.sep@gmail.com>
2020-01-15 17:59:58 +0800

    feat: BinarySearch problem : float sqrt(x, double epsilon)
    key points of implementation:
    - left & right pointer assign should NOT + 1 (for float reason)
    - care about the float "equal" expression
    - care about the int overflow case
    - care about the special case 0 & 1

--- src/main/java/me/lirui/algo/binarysearch/LeetCodeBinarySearchSolution.java
+++ src/main/java/me/lirui/algo/binarysearch/LeetCodeBinarySearchSolution.java
@@ -5,29 +5,97 @@ class LeetCodeBinarySearchSolution {
   //  Sqrt(x)
   // https://leetcode.com/problems/sqrtx/
   //
+
+  // ======================================================
+  // When integer overflow occurred?
+  //
+  // some mathematical operation may cause the overflow,  such as the "+" and "*"
+  // big integer number will cause the overflow.
+  //
+  //
+  // How to deal with the integer overflow?
+  //
+  //    the following tricks to deal with the possible int overflow,
+  //
+  //    1. level up the current parameter to next range level
+  //        for example int           -->   long
+  //    2. substitute the possible overflow equation with other one
+  //        mid = (left + right) / 2  -->   mid = left + (right - left) / 2
+  //        mif * mid == x            -->   mid == x / mid
+
+  //
+  // Level up trick: (the following code also pass the test cases)
+  // ---------------
+  //
+  //  int mySqrt(int x) {
+  //    return (int) _mySqrt(x);
+  //  }
+  //
+  //  private long _mySqrt(long x) {
+  //    if (x < 0) return -1;
+  //
+  //    long left = 0, right = x;
+  //    long result = 0;
+  //    while (left <= right) {
+  //      long mid = left + ((right - left) >> 1);
+  //      if (mid * mid == x) return mid;
+  //      if (mid * mid < x) {
+  //        left = mid + 1;
+  //        result = mid;
+  //      }
+  //      if (mid * mid > x) right = mid - 1;
+  //    }
+  //
+  //    return result;
+  //  }
+  //
+
+  //
+  // mathematical substitution trick:
+  // ---------------
+
   int mySqrt(int x) {
-    return (int) _mySqrt(x);
-  }
-
-  private long _mySqrt(long x) {
     if (x < 0) return -1;
-
-    long left = 0, right = x;
-    long result = 0;
+    if (x == 0 || x == 1) return x;
+    int left = 0;
+    int right = x;
+    int result = 0;
     while (left <= right) {
-      long mid = left + (right - left) / 2;
-      if (mid * mid == x) return mid;
-      if (mid * mid < x) {
+      int mid = left + (right - left) / 2;
+      if (mid == x / mid) return mid;
+      else if (mid > x / mid) right = mid - 1;
+      else {
         left = mid + 1;
         result = mid;
       }
-      if (mid * mid > x) right = mid - 1;
     }
 
     return result;
   }
+
   //
-  //  Sqrt(x)
+  //  Sqrt(x, precision)
   // https://leetcode.com/problems/sqrtx/
+  // End
+
+  //
+  //  float Sqrt(x, epsilon)
+  //
+  float mSqrt(int x, double epsilon) {
+    if (x < 0) return -1;
+    if (x == 0 || x == 1) return x;
+
+    float left = 0, right = x;
+    while (left <= right) {
+      float mid = left + (right - left) / 2;
+      if (Math.abs(mid - x / mid) < epsilon) return mid;
+      if (mid < x / mid) left = mid;
+      if (mid > x / mid) right = mid;
+    }
+
+    return Float.NEGATIVE_INFINITY;
+  }
+  //
+  //  int Sqrt(x, precision)
   // End
 }
--- src/test/java/me/lirui/algo/binarysearch/LeetCodeBinarySearchSolutionTest.java
+++ src/test/java/me/lirui/algo/binarysearch/LeetCodeBinarySearchSolutionTest.java
@@ -26,6 +26,12 @@ public class LeetCodeBinarySearchSolutionTest {
   }
 
   @Test
+  public void mySqrtZero() {
+    int sqr = mSolution.mySqrt(0);
+    Assert.assertEquals(0, sqr);
+  }
+
+  @Test
   public void mySqrtBigInt() {
     int sqr = mSolution.mySqrt(2147395599);
     Assert.assertEquals(46339, sqr);
@@ -35,5 +41,35 @@ public class LeetCodeBinarySearchSolutionTest {
   public void mySqrtInvalid() {
     int sqr = mSolution.mySqrt(-1);
     Assert.assertEquals(-1, sqr);
+  }
+
+  @Test
+  public void mSqrt() {
+    float sqr = mSolution.mSqrt(4, 0.00001);
+    Assert.assertTrue(Math.abs(sqr - 2) <= 0.00001);
+  }
+
+  @Test
+  public void mSqrtRound() {
+    float sqr = mSolution.mSqrt(8, 0.00001);
+    Assert.assertTrue(Math.abs(sqr - 2.82843) <= 0.00001);
+  }
+
+  @Test
+  public void mSqrtZero() {
+    float sqr = mSolution.mSqrt(0, 0.00001);
+    Assert.assertTrue(Math.abs(sqr - 0) <= 0.00001);
+  }
+
+  @Test
+  public void mSqrtBigInt() {
+    float sqr = mSolution.mSqrt(2147395599, 0.00001);
+    Assert.assertTrue(Math.abs(sqr - 46340.00000) <= 0.00001);
+  }
+
+  @Test
+  public void mSqrtInvalid() {
+    float sqr = mSolution.mSqrt(-1, 0.00001);
+    Assert.assertTrue(Math.abs(sqr + 1.0) < 0.00001);
   }
 }
