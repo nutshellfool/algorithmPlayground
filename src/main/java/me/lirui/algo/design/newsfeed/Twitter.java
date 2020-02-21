05f4d76a66e26f6d23b014f28ea18720c6f827b2
李瑞 <lirui.sep@gmail.com>
2020-02-21 10:27:31 +0800

    feat: feat Design problem :  Design Twitter
    A "pull on demand" message triger implementation:
    
    key points of
    implementation:
    - every user maintain a "out" tweet box
    - every user
    maintain a "follow" user set
    - merge k tweet list on getTweet
    operation
    - optimazition for getTweet operation use
    PriorityQueue(MaxHeap) for "latest k" tweet problem
    
    corner case should
    take care:
    - user should follow himself(herself)
    - user can not unfollow
    himself(herself)
    
    Closes https://leetcode.com/problems/design-twitter/

---
+++ src/main/java/me/lirui/algo/design/newsfeed/Twitter.java
@@ -0,0 +1,128 @@
+package me.lirui.algo.design.newsfeed;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.TreeSet;
+
+// Design Twitter
+// https://leetcode.com/problems/design-twitter/
+/**
+ * Your Twitter object will be instantiated and called as such: Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId); List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId); obj.unfollow(followerId,followeeId);
+ */
+class Twitter {
+
+  // Actually, this implementation is a "pull on demand" mode,
+  // "push on change" mode is another topic.
+  //
+
+  private HashMap<Integer, ArrayList<Tweet>> userTweetListMap;
+  private HashMap<Integer, Set<Integer>> userFollowMap;
+  private int timeSequence;
+
+  /**
+   * Initialize your data structure here.
+   */
+  Twitter() {
+    userTweetListMap = new HashMap<>();
+    userFollowMap = new HashMap<>();
+    timeSequence = 0;
+  }
+
+  /**
+   * Compose a new tweet.
+   */
+  void postTweet(int userId, int tweetId) {
+    ArrayList<Tweet> tweetList = userTweetListMap.get(userId);
+    if (tweetList == null) {
+      tweetList = new ArrayList<>();
+      userTweetListMap.put(userId, tweetList);
+      // follow himself first
+      follow(userId, userId);
+    }
+
+    tweetList.add(new Tweet(tweetId, timeSequence++));
+
+  }
+
+  /**
+   * Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must
+   * be posted by users who the user followed or by the user herself. Tweets must be ordered from
+   * most recent to least recent.
+   */
+  List<Integer> getNewsFeed(int userId) {
+    Set<Integer> followList = userFollowMap.get(userId);
+    if (followList == null) {
+      return new ArrayList<>();
+    }
+
+    PriorityQueue<Tweet> maxHeap = new PriorityQueue<>(followList.size(),
+        new Comparator<Tweet>() {
+          @Override
+          public int compare(Tweet o1, Tweet o2) {
+            return o2.timeElapsed - o1.timeElapsed;
+          }
+        });
+
+    for (int followerId : followList) {
+      List<Tweet> tweets = userTweetListMap.get(followerId);
+      // Here is a test case
+      if (tweets == null) {
+        continue;
+      }
+
+      for (Tweet tweet : tweets) {
+        maxHeap.offer(tweet);
+      }
+    }
+
+    List<Integer> newsFeeds = new ArrayList<>(maxHeap.size());
+    while (newsFeeds.size() < 10 && !maxHeap.isEmpty()) {
+      newsFeeds.add(maxHeap.poll().tweetId);
+    }
+
+    return newsFeeds;
+  }
+
+  /**
+   * Follower follows a followee. If the operation is invalid, it should be a no-op.
+   */
+  void follow(int followerId, int followeeId) {
+    Set<Integer> followList = userFollowMap.get(followerId);
+    if (followList == null) {
+      followList = new TreeSet<>();
+      userFollowMap.put(followerId, followList);
+    }
+    followList.add(followeeId);
+  }
+
+  /**
+   * Follower unfollows a followee. If the operation is invalid, it should be a no-op.
+   */
+  void unfollow(int followerId, int followeeId) {
+    Set<Integer> followList = userFollowMap.get(followerId);
+    // user can not unfollow himself
+    if (followList != null && followerId != followeeId) {
+      followList.remove(followeeId);
+    }
+  }
+
+  class Tweet {
+
+    int tweetId;
+    int timeElapsed;
+
+    Tweet(int tweetId, int timeElapsed) {
+      this.tweetId = tweetId;
+      this.timeElapsed = timeElapsed;
+    }
+  }
+}
+// Design Twitter
+// https://leetcode.com/problems/design-twitter/
+// End
---
+++ src/test/java/me/lirui/algo/design/newsfeed/TwitterTest.java
@@ -0,0 +1,98 @@
+package me.lirui.algo.design.newsfeed;
+
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TwitterTest {
+
+  private Twitter mTwitter;
+
+  @Before
+  public void setUp() {
+    mTwitter = new Twitter();
+  }
+
+  @Test
+  public void happyCaseTest() {
+    mTwitter.postTweet(1, 5);
+    List<Integer> feedIds = mTwitter.getNewsFeed(1);
+    Assert.assertNotNull(feedIds);
+    Assert.assertEquals(1, feedIds.size());
+    Assert.assertEquals(5, feedIds.get(0));
+
+    mTwitter.follow(1, 2);
+    mTwitter.postTweet(2, 6);
+
+    List<Integer> feedIds1 = mTwitter.getNewsFeed(1);
+    Assert.assertNotNull(feedIds1);
+    Assert.assertEquals(2, feedIds1.size());
+    Assert.assertEquals(6, feedIds1.get(0));
+    Assert.assertEquals(5, feedIds1.get(1));
+  }
+
+  @Test
+  public void doNothingAndGetFeedIds() {
+    List<Integer> feedIds = mTwitter.getNewsFeed(1);
+    Assert.assertNotNull(feedIds);
+    Assert.assertEquals(0, feedIds.size());
+  }
+
+  @Test
+  public void followeeNoPostCaseTest() {
+    mTwitter.follow(1, 3);
+    List<Integer> feedIds = mTwitter.getNewsFeed(1);
+    Assert.assertNotNull(feedIds);
+    Assert.assertEquals(0, feedIds.size());
+  }
+
+  @Test
+  public void feedLengthHappyCase() {
+    for (int i = 0; i < 10; i++) {
+      mTwitter.postTweet(1, 10 + i);
+    }
+
+    List<Integer> feeds = mTwitter.getNewsFeed(1);
+    Assert.assertNotNull(feeds);
+    Assert.assertEquals(10, feeds.size());
+  }
+
+  @Test
+  public void feedLengthMoreThanBoundaryCase() {
+    for (int i = 0; i < 11; i++) {
+      mTwitter.postTweet(1, 10 + i);
+    }
+
+    List<Integer> feeds = mTwitter.getNewsFeed(1);
+    Assert.assertNotNull(feeds);
+    Assert.assertEquals(10, feeds.size());
+    Assert.assertEquals(20, feeds.get(0));
+    Assert.assertEquals(19, feeds.get(1));
+  }
+
+  @Test
+  public void testUnfollow() {
+    mTwitter.postTweet(1, 5);
+    mTwitter.follow(1, 3);
+    mTwitter.postTweet(3, 6);
+
+    mTwitter.unfollow(1, 3);
+
+    List<Integer> feedIds2 = mTwitter.getNewsFeed(1);
+
+    Assert.assertNotNull(feedIds2);
+    Assert.assertEquals(1, feedIds2.size());
+    Assert.assertEquals(5, feedIds2.get(0));
+  }
+
+  @Test
+  public void testUnfollowSelf() {
+    mTwitter.postTweet(2, 5);
+    mTwitter.unfollow(2, 2);
+    List<Integer> feeds = mTwitter.getNewsFeed(2);
+    Assert.assertNotNull(feeds);
+    Assert.assertEquals(1, feeds.size());
+    Assert.assertEquals(5, feeds.get(0));
+  }
+}
