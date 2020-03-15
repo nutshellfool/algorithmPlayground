30d72d49e19861a1812fecc7d08fa6d97b58e093
李瑞 <lirui.sep@gmail.com>
2020-03-15 09:27:24 +0800

    refactor: refactor  LinkedList problem : Add Two Numbers
    optimize code implementation:
    
    - the parameter null check guardian is
    redundant
    - the last carry process can be merged in main process logic
    code

--- src/main/java/me/lirui/algo/linkedlist/LeetCodeLinkedListSolution.java
+++ src/main/java/me/lirui/algo/linkedlist/LeetCodeLinkedListSolution.java
@@ -4,27 +4,20 @@ import java.util.Stack;
 
 class LeetCodeLinkedListSolution {
 
+  // Add Two Numbers
+  // https://leetcode.com/problems/add-two-numbers/
   SingleLinkedNode addTwoNumbers(SingleLinkedNode l1, SingleLinkedNode l2) {
-    if (l1 == null && l2 == null) {
-      return null;
-    }
-
     SingleLinkedNode newDummyHead = new SingleLinkedNode();
     SingleLinkedNode resultNode = newDummyHead;
     SingleLinkedNode l1Pointer = l1;
     SingleLinkedNode l2pointer = l2;
     int carry = 0;
-
-    while (l1Pointer != null || l2pointer != null) {
-
-      // do the calculation work
+    while (l1Pointer != null || l2pointer != null || carry != 0) {
       int sum = carry;
-
       if (l1Pointer != null) {
         sum += l1Pointer.getValue();
         l1Pointer = l1Pointer.next;
       }
-
       if (l2pointer != null) {
         sum += l2pointer.getValue();
         l2pointer = l2pointer.next;
@@ -37,25 +30,21 @@ class LeetCodeLinkedListSolution {
       carry = sum / 10;
     }
 
-    if (carry == 1) {
-      SingleLinkedNode node = new SingleLinkedNode();
-      node.setValue(1);
-      resultNode.next = node;
-    }
-
     return newDummyHead.next;
   }
+  // Add Two Numbers
+  // https://leetcode.com/problems/add-two-numbers/
+  // End
 
+  // Add Two Numbers II
+  // https://leetcode.com/problems/add-two-numbers-ii/
   SingleLinkedNode addTwoNumbers2(SingleLinkedNode l1, SingleLinkedNode l2) {
-
     Stack<Integer> l1Stack = new Stack<>();
     Stack<Integer> l2Stack = new Stack<>();
-
     while (l1 != null) {
       l1Stack.push(l1.getValue());
       l1 = l1.next;
     }
-
     while (l2 != null) {
       l2Stack.push(l2.getValue());
       l2 = l2.next;
@@ -63,11 +52,8 @@ class LeetCodeLinkedListSolution {
 
     SingleLinkedNode dummyNode = null;
     int carry = 0;
-
     while (!l1Stack.empty() || !l2Stack.empty() || carry != 0) {
-
       int sum = carry;
-
       if (!l1Stack.empty()) {
         sum += l1Stack.pop();
       }
@@ -79,10 +65,12 @@ class LeetCodeLinkedListSolution {
       newHead.setValue(sum % 10);
       newHead.next = dummyNode;
       dummyNode = newHead;
-
       carry = sum / 10;
     }
 
     return dummyNode;
   }
+  // Add Two Numbers II
+  // https://leetcode.com/problems/add-two-numbers-ii/
+  // End
 }
