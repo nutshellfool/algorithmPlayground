7e6303879ad223f9e2fd84f96b7cb1735b737117
李瑞 <lirui.sep@gmail.com>
2020-04-06 14:58:31 +0800

    feat: BinarySearch problem : lower bound
    A lower bound version Binary Search problem implementation:
    
    Key points
    of implementation:
    
    care the lower bound case logic branch, especially
    the index should not over low bound.

--- src/main/java/me/lirui/algo/binarysearch/BinarySearch.java
+++ src/main/java/me/lirui/algo/binarysearch/BinarySearch.java
@@ -1,19 +1,28 @@
 package me.lirui.algo.binarysearch;
 
 class BinarySearch {
+
   //  Binary Search
   // https://leetcode.com/problems/binary-search/
   //
   int binarySearchStandard(int[] array, int target) {
-    if (array == null || array.length == 0) return -1;
+    if (array == null || array.length == 0) {
+      return -1;
+    }
 
     int left = 0, right = array.length - 1;
 
     while (left <= right) {
       int mid = left + (right - left) / 2;
-      if (array[mid] == target) return mid;
-      if (array[mid] < target) left = mid + 1;
-      if (array[mid] > target) right = mid - 1;
+      if (array[mid] == target) {
+        return mid;
+      }
+      if (array[mid] < target) {
+        left = mid + 1;
+      }
+      if (array[mid] > target) {
+        right = mid - 1;
+      }
     }
 
     return -1;
@@ -21,4 +30,28 @@ class BinarySearch {
   //  Binary Search
   // https://leetcode.com/problems/binary-search/
   // End
+
+  int binarySearchLowerBound(int[] array, int target) {
+    if (array == null || array.length == 0) {
+      return -1;
+    }
+
+    int left = 0, right = array.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (array[mid] < target) {
+        left = mid + 1;
+      } else if (array[mid] > target) {
+        right = mid - 1;
+      } else {
+        if ((mid == 0) || array[mid - 1] != target) {
+          return mid;
+        } else {
+          right = mid - 1;
+        }
+      }
+    }
+
+    return -1;
+  }
 }
--- src/test/java/me/lirui/algo/binarysearch/BinarySearchTest.java
+++ src/test/java/me/lirui/algo/binarysearch/BinarySearchTest.java
@@ -41,6 +41,34 @@ public class BinarySearchTest {
   public void binarySearchStandNotExists() {
     int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int position = mSolution.binarySearchStandard(array, 11);
-    Assert.assertEquals(position, -1);
+    Assert.assertEquals(-1, position);
+  }
+
+  @Test
+  public void binarySearchLowerBound() {
+    int[] array = {1, 2, 3, 4, 5, 6, 6, 6, 7, 8};
+    int position = mSolution.binarySearchLowerBound(array, 6);
+    Assert.assertEquals(5, position);
+  }
+
+  @Test
+  public void binarySearchLowerBoundFront() {
+    int[] array = {1, 2, 3, 4, 5, 6, 6, 6, 7, 8};
+    int position = mSolution.binarySearchLowerBound(array, 1);
+    Assert.assertEquals(0, position);
+  }
+
+  @Test
+  public void binarySearchLowerBoundEnd() {
+    int[] array = {1, 2, 3, 4, 5, 6, 6, 6, 7, 8};
+    int position = mSolution.binarySearchLowerBound(array, 8);
+    Assert.assertEquals(9, position);
+  }
+
+  @Test
+  public void binarySearchLowerBoundNotExists() {
+    int[] array = {1, 2, 3, 4, 5, 6, 6, 6, 7, 8};
+    int position = mSolution.binarySearchLowerBound(array, 10);
+    Assert.assertEquals(-1, position);
   }
 }
